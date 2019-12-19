d9a5064dfe49db6cab66900a5a54c6b870a918ec
Junior <junior.jaava@gmail.com>
2019-12-19 17:48:06 +0100

    + returning a object as response

--- 02.restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ 02.restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -19,21 +19,21 @@ public class HelloWorldController {
 		return "Hello World";
 	}
 
-//	@GetMapping(path = "/hello-world-bean")
-//	public HelloWorldBean helloWorldBean() {
-//		return new HelloWorldBean("Hello World");
-//	}
-//
-//	///hello-world/path-variable/in28minutes
-//	@GetMapping(path = "/hello-world/path-variable/{name}")
-//	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
-//		return new HelloWorldBean(String.format("Hello World, %s", name));
-//	}
-//
-//	@GetMapping(path = "/hello-world-internationalized")
-//	public String helloWorldInternationalized() {
-//		return messageSource.getMessage("good.morning.message", null,
-//									LocaleContextHolder.getLocale());
-//	}
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+
+	///hello-world/path-variable/in28minutes
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null,
+									LocaleContextHolder.getLocale());
+	}
 
 }
