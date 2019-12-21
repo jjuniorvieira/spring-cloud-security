e961bf6ed28bc8050db714b1c1c42c684d4b8284
Junior <junior.jaava@gmail.com>
2019-12-21 21:35:22 +0100

    + implementing hateos concept, to create a link to other endpoint in a response: {     "id": 1,     "name": "Adam",     "birthDate": "2019-12-21T20:29:27.551+0000",     "posts": null,     "_links": {         "all-users": {             "href": "http://localhost:8080/users"         }     } }

--- 02.restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ 02.restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,13 +1,17 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
@@ -31,13 +35,17 @@ public class UserResource {
 
 
 	@GetMapping("/users/{id}")
-		public ResponseEntity<User> retrieveUser(@PathVariable int id) {
+	public EntityModel<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
+		EntityModel<User> entity = new EntityModel<>(user);
 
 		if(user==null)
 			throw new UserNotFoundException("id-"+ id);
 
-		return ResponseEntity.ok(user);
+		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entity.add(linkTo.withRel("all-users"));
+
+		return entity;
 	}
 
 	//
