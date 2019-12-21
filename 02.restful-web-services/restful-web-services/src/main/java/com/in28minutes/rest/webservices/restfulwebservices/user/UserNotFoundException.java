4805f76292eab4c1a4e98d3b37626183a0ce6bc4
Junior <junior.jaava@gmail.com>
2019-12-21 17:43:32 +0100

    + return ResponseStatus annotation + return not found code error

--- 02.restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserNotFoundException.java
+++ 02.restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserNotFoundException.java
@@ -3,7 +3,7 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_FOUND)   //annotations response status
 public class UserNotFoundException extends RuntimeException {
 	public UserNotFoundException(String message) {
 		super(message);
