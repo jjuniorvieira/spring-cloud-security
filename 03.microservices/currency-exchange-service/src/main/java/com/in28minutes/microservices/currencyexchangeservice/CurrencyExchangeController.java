070834f21f82a18b6349ea0ea1d60c7ad48db3ea
Junior <junior.jaava@gmail.com>
2020-01-15 20:23:39 +0100

    + Create a simple hard coded currency exchange service   + returning hardcoded

--- 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -8,29 +8,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
 @RestController
 public class CurrencyExchangeController {
-	
+
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private Environment environment;
-	
+
 	@Autowired
 	private ExchangeValueRepository repository;
-	
+
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue
 		(@PathVariable String from, @PathVariable String to){
-		
-		ExchangeValue exchangeValue = 
-				repository.findByFromAndTo(from, to);
-		
-		exchangeValue.setPort(
-				Integer.parseInt(environment.getProperty("local.server.port")));
-		
-		logger.info("{}", exchangeValue);
-		
-		return exchangeValue;
+//
+//		ExchangeValue exchangeValue =
+//				repository.findByFromAndTo(from, to);
+//
+//		exchangeValue.setPort(
+//				Integer.parseInt(environment.getProperty("local.server.port")));
+//
+//		logger.info("{}", exchangeValue);
+//
+//		return exchangeValue;
+
+		return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
 	}
 }
