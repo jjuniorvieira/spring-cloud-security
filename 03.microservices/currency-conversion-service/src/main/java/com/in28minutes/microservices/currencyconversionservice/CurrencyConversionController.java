3704a864aa7fa9398b62628256428dd4b1369453
Junior <junior.jaava@gmail.com>
2020-01-18 17:22:31 +0100

    + Creating a service for currency conversion ( using feign, netflix, ribbon and etc )   + hardcoded value sent

--- 03.microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ 03.microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -18,8 +18,8 @@ public class CurrencyConversionController {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	@Autowired
-	private CurrencyExchangeServiceProxy proxy;
+//	@Autowired
+//	private CurrencyExchangeServiceProxy proxy;
 
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
@@ -43,16 +43,16 @@ public class CurrencyConversionController {
 		return new CurrencyConversionBean(1L, from, to, BigDecimal.ONE, quantity, quantity, 0);
 	}
 
-	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
-                                                       @PathVariable BigDecimal quantity) {
-
-		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
-
-		logger.info("{}", response);
-		
-		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
-				quantity.multiply(response.getConversionMultiple()), response.getPort());
-	}
+//	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+//	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
+//                                                       @PathVariable BigDecimal quantity) {
+//
+//		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+//
+//		logger.info("{}", response);
+//
+//		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+//				quantity.multiply(response.getConversionMultiple()), response.getPort());
+//	}
 
 }
