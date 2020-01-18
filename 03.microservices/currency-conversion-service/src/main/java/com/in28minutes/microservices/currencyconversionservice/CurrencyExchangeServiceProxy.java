c64893b05d3389f6b800a49d1c618f65377a8527
Junior <junior.jaava@gmail.com>
2020-01-18 18:07:50 +0100

    + Using Feign REST Client for Service Invocation

--- 03.microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ 03.microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange-service", url="localhost:8000")
-//@FeignClient(name="currency-exchange-service")
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
 //@FeignClient(name="netflix-zuul-api-gateway-server")
 //@RibbonClient(name="currency-exchange-service")
+
+@FeignClient(name="currency-exchange-service", url="localhost:8000")
 public interface CurrencyExchangeServiceProxy {
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 //	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue (@PathVariable("from") String from, @PathVariable("to") String to);
 }
+
+//@FeignClient(name="currency-exchange-service")
