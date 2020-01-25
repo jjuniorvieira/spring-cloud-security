297c2a138593e3b9b976fc5e140f6e0e2de4467f
Junior <junior.jaava@gmail.com>
2020-01-25 14:46:01 +0100

    + now to currency conversion call currency exchange, the call going throw netflix-zuul-api-gateway-server

--- 03.microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ 03.microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -6,14 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange-service", url="localhost:8000")
-//@FeignClient(name="netflix-zuul-api-gateway-server")
-
-@FeignClient(name="currency-exchange-service")
+//@FeignClient(name="currency-exchange-service") now to currency conversion call currency exchange, the call going throw netflix-zuul-api-gateway-server
+@FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service") //getting these values from the properties.
 public interface CurrencyExchangeServiceProxy {
-//	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+//	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue (@PathVariable("from") String from, @PathVariable("to") String to);
 }
-
-//@FeignClient(name="currency-exchange-service")
