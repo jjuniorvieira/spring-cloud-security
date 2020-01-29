eb4036ff27dfeca93bd73ac381f281718e0a4a7e
Junior <junior.jaava@gmail.com>
2020-01-29 21:07:24 +0100

    + Adding configuration to use Hystrix (Fault Tolerance services) + Adding default method in case the service is down

--- 03.microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ 03.microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -2,6 +2,7 @@ package com.in28minutes.microservices.limitsservice;
 
 import com.in28minutes.microservices.limitsservice.bean.LimitConfiguration;
 //import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,10 @@ public class LimitsConfigurationController {
 		LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 		return limitConfiguration;
 	}
-	
+
+	//if for some hall this service is unavailable, the default method to be called is: fallbackRetrieveConfiguration
 	@GetMapping("/fault-tolerance-example")
-	//@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
 	public LimitConfiguration retrieveConfiguration() {
 		throw new RuntimeException("Not available");
 	}
